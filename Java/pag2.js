77941cb544affd778e4943a488ca73cc12929ea1
Filipe Wesley <125272841+hardze@users.noreply.github.com>
2024-09-01 14:48:40 -0300

    Delete Java directory

--- Java/pag2.js
+++
@@ -1,8 +0,0 @@
-{let play = document.getElementById("icon_fone");
-
-function reproduzSom() {
-  let audio = new Audio("audios/cadeira.mp3");
-  audio.play()
-}
-play.addEventListener("click", reproduzSom);
-}
