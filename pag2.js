8e14ea3e0329f0f6186ae7d59c1197f747bb7e7e
Filipe Wesley <125272841+hardze@users.noreply.github.com>
2024-09-01 14:45:12 -0300

    Delete pag2.js

--- pag2.js
+++
@@ -1,7 +0,0 @@
-let play = document.getElementById("icon_fone");
-
-function reproduzSom() {
-  let audio = new Audio("audios/Nota.mp3");
-  audio.play()
-}
-play.addEventListener("click", reproduzSom);
