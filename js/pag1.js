7f8d88485cf49b21c646dfac2cf37368588bfc52
Filipe Wesley <125272841+hardze@users.noreply.github.com>
2024-09-07 13:35:40 -0300

    Add files via upload

--- js/pag1.js
+++ js/pag1.js
@@ -1,13 +1,11 @@
-{let play = document.getElementById("icon_fone");
+let play = document.getElementById("icon_fone");
 
 function reproduzSom() {
   let audio = new Audio("audios/computador.mp3");
   audio.play()
 }
   play.addEventListener("click", reproduzSom);
-}
 
-let avancar = document.getElementById("avancar");
 
   function avancar(){
   var letra = document.getElementById('letra1').value;
@@ -18,15 +16,19 @@ let avancar = document.getElementById("avancar");
   var letra6 = document.getElementById('letra6').value;
   var letra7 = document.getElementById('letra7').value;
   var letra8 = document.getElementById('letra8').value;
+  var letra9 = document.getElementById('letra9').value;
+  var letra10 = document.getElementById('letra10').value;
 
-  if(letra == "u" && 
-  letra2 == "n" && 
-  letra3 == "i" && 
-  letra4 == "f" && 
-  letra5 == "o" &&
-  letra6 == "r" &&
-  letra7 == "m" &&
-  letra8 == "e"){
+  if(letra == "c" && 
+  letra2 == "o" && 
+  letra3 == "m" && 
+  letra4 == "p" && 
+  letra5 == "u" &&
+  letra6 == "t" &&
+  letra7 == "a" &&
+  letra8 == "d" &&
+  letra9 == "o" &&
+  letra10 == "r"){
     alert('Palavra correta')
     location.href = "pag2.html";
   }
--- js/pag2.js
+++ js/pag2.js
@@ -1,8 +1,31 @@
-{let play = document.getElementById("icon_fone");
+let play = document.getElementById("icon_fone");
 
 function reproduzSom() {
   let audio = new Audio("audios/cadeira.mp3");
   audio.play()
 }
-play.addEventListener("click", reproduzSom);
-}
+  play.addEventListener("click", reproduzSom);
+
+  function avancar(){
+    var letra = document.getElementById('letra1').value;
+    var letra2 = document.getElementById('letra2').value;
+    var letra3 = document.getElementById('letra3').value;
+    var letra4 = document.getElementById('letra4').value;
+    var letra5 = document.getElementById('letra5').value;
+    var letra6 = document.getElementById('letra6').value;
+    var letra7 = document.getElementById('letra7').value;
+  
+    if(letra == "c" && 
+    letra2 == "a" && 
+    letra3 == "d" && 
+    letra4 == "e" && 
+    letra5 == "i" &&
+    letra6 == "r" &&
+    letra7 == "a"){
+      alert('Palavra correta')
+      location.href = "pag1.html";
+    }
+    else{
+      alert('Palavra incorreta')
+    }
+  }
